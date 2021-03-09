4828b3b2052da51ed951738ef0a32cb5e51d39ec
Suraj <info.blogtheorem@gmail.com>
2021-03-09 19:44:43 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  /*
-
-  YOUR FIREBASE CONFIG SETUP
-
-  */
+  apiKey: "AIzaSyAUpaB7HMHYkAGMaQBd6yI9kRa626Zs_nI",
+  authDomain: "linkedin-clone-57f16.firebaseapp.com",
+  projectId: "linkedin-clone-57f16",
+  storageBucket: "linkedin-clone-57f16.appspot.com",
+  messagingSenderId: "402559157796",
+  appId: "1:402559157796:web:4f61ffd079a69ad4033ab1",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
